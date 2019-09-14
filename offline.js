cbbd9d2469ca61a7f2ecbfd0d0714038bacbebd3
Karma-Ctrl <55327501+Karma-Ctrl@users.noreply.github.com>
2019-09-14 13:35:41 -0400

    Add files via upload XD

---
+++ offline.js
@@ -0,0 +1,14 @@
+﻿{
+	"version": 1568482211,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/spritefont.png",
+		"images/leftfloor.png",
+		"images/sand.png",
+		"images/net.png",
+		"images/choosescore1.png"
+	]
+}
